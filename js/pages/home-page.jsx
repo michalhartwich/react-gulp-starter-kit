a84c9b218621df5c6a7102316792bbeec02a447b
Michał Hartwich <michal.hartwich@netguru.pl>
2016-02-05 23:50:13 +0100

    include red text sample component

--- js/pages/home-page.jsx
+++ js/pages/home-page.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
+import RedText from '../components/red-text';
+
 class HomePage extends Component {
   render() {
     return (
       <div>
         <h1>Home page</h1>
+        <RedText text='This is sample red text component.' />
         <Link to='/sample'>Go to sample page</Link>
       </div>
     )
